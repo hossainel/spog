27dd21359411d999eb84d052848cb8050c079c70
Md Safayet El Hossain <31356820+hossainel@users.noreply.github.com>
2019-11-11 15:57:21 +0600

    Needed to modify later

---
+++ classical/ABSYS - Anti-Blot System.py
@@ -0,0 +1,11 @@
+#number + number = number
+def divider(n):
+    #a + b = c
+    a = n.split('+')[0]
+    b, c = n.split('+')[1].split('=')
+    if 'machula' in a: print(str(int(c)-int(b))+' +'+b+'='+c)
+    elif 'machula' in b: print(a+'+ '+str(int(c)-int(a))+' ='+c)
+    else: print(a+'+'+b+'= '+str(int(a)+int(b)))
+inp = int(input())
+for i in range(inp):
+    divider(input())
