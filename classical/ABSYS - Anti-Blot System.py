19c512789b847faee10c4166cb87a161846d7f78
Md Safayet El Hossain <31356820+hossainel@users.noreply.github.com>
2019-11-11 19:04:51 +0600

    Solved

--- classical/ABSYS - Anti-Blot System.py
+++ classical/ABSYS - Anti-Blot System.py
@@ -3,9 +3,19 @@ def divider(n):
     #a + b = c
     a = n.split('+')[0]
     b, c = n.split('+')[1].split('=')
-    if 'machula' in a: print(str(int(c)-int(b))+' +'+b+'='+c)
-    elif 'machula' in b: print(a+'+ '+str(int(c)-int(a))+' ='+c)
-    else: print(a+'+'+b+'= '+str(int(a)+int(b)))
+    if 'machula' in a:
+        print(str(int(c)-int(b))+' +'+b+'='+c)
+    elif 'machula' in b:
+        print(a+'+ '+str(int(c)-int(a))+' ='+c)
+    else:
+        print(a+'+'+b+'= '+str(int(a)+int(b)))
+        
 inp = int(input())
+input()
 for i in range(inp):
-    divider(input())
+    d = input()
+    divider(d)
+    if i==inp-1:
+        pass
+    else:
+        input()
